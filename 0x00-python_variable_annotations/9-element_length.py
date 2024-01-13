2701783de5bd2f68fece168fa41d5db060de1e9d
PeterEkwere <peterekwere@icloud.com>
2024-01-13 18:24:37 +0100

    2nd update for task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,12 @@ from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ This function takes a list of strings
+
+    Args:
+        lst (Iterable[Sequence]): _description_
+
+    Returns:
+        List[Tuple[Sequence, int]]: _description_
+    """
     return [(i, len(i)) for i in lst]

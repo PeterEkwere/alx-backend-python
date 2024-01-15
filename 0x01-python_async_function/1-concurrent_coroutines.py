a3d418852ddb1ac6f828ff7b9963cc05d30728a6
PeterEkwere <peterekwere@icloud.com>
2024-01-15 22:36:25 +0100

    initial commit for task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    This module contains basic async practices
+    Author: Peter Ekwere
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ This function will spawn wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): an integer
+        max_delay (int): an integer
+
+    Returns:
+        List: This is a list of all delays
+    """
+    a_list = []
+    for i in range(n):
+        a_list.append(wait_random(max_delay))
+    earliests = []
+    for i in asyncio.as_completed(a_list):
+        earliest = await i
+        earliests.append(earliest)
+    return earliests

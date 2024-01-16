59bc8271dd9e58c90d2944d318b0a68702ca8e85
PeterEkwere <peterekwere@icloud.com>
2024-01-16 21:59:59 +0100

    initial commit for task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,8 +7,9 @@ import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List:
-    """ This Function returns will collect 10 random numbers using an async comprehensing over async_generator
+    """ async comprehensing over async_generator
 
     Returns:
         List:
